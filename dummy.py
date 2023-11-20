2eb55b155c528f923a9a5a4c51dfa00197c5573b
Prateek <prateek.kesarwani@sap.com>
2023-11-20 17:21:10 +0530

    fdifhg

--- dummy.py
+++ dummy.py
@@ -11,7 +11,6 @@ logger = structlog.get_logger(__name__)
 GITHUB_REMOTE = "git@github.com:"
 ORIGIN = "origin"
 
-obj=PullRequestAutomationService()
 
 class PullRequestForNewRepos(PullRequestAutomationService):
   
@@ -29,33 +28,33 @@ class PullRequestForNewRepos(PullRequestAutomationService):
         cutoff_date = datetime.now() - timedelta(days=60)
         repocount_tracker = 0
 
-        logger.info(f"Filtering repositories in org: {obj.org_name} by creation time asc and creating PRs.")
+        logger.info(f"Filtering repositories in org: {self.org} by creation time asc and creating PRs.")
         print(type(self.org))
 
-        for repo in obj.org.get_repos(direction="desc", sort="created", type="all"):
+        for repo in self.org.get_repos(direction="desc", sort="created", type="all"):
             repocount_tracker += 1
             if repo == "manser-test":
                 creation_date = repo.created_at.replace(tzinfo=None)
 
                 if creation_date >= cutoff_date:
                     try:
-                        obj.set_gitlink_n_repopath(repo.name)
-                        obj.clone_repository(repo.name)
-                        obj.commit_and_push()
-                        obj.create_pr(repo)
+                        self.set_gitlink_n_repopath(repo.name)
+                        self.clone_repository(repo.name)
+                        self.commit_and_push()
+                        self.create_pr(repo)
                 
                     except GithubException as e:
                         raise e
                 
     # Check if all PRs are done for all repositories in the organization
-        obj.check_if_all_prs_done(repocount_tracker)
+        self.check_if_all_prs_done(repocount_tracker)
 
             
 if __name__ == "__main__":
     logger.info("Starting pull request creation for Managed Services GitHub mirror automation...")
 
     pr_service = PullRequestForNewRepos()
-    pr_service.base_branch_name = "main"
+    # pr_service.base_branch_name = "main"
     pr_service.create_prs_in_batches()
 
     logger.info('Successfuly completed PR generation for this run..🎉🎉🎉 ')
