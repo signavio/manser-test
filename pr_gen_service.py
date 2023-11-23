7d9001d93dcf8901901ce0684a5e3b66cc9ffd9e
Prateek <prateek.kesarwani@sap.com>
2023-11-23 13:57:10 +0530

    reyyreuiqweyqqweqerer

--- pr_gen_service.py
+++ pr_gen_service.py
@@ -105,7 +105,7 @@ class PullRequestAutomationService(RemoteProgress):
         print(curr_dir)
         logger.info(f"Current dir: {curr_dir}")
         print(file_to_sync)
-        dir_name = os.path.join(curr_dir, file_to_sync)
+        dir_name = os.path.join(file_to_sync, curr_dir)
         # dir_name = os.path.join(curr_dir, "/", file_to_sync)
         print("Hello")
         print(dir_name)
