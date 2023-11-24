0a236fc6a4c76e6ce51a4f3adc707b24dc8b73b8
Prateek <prateek.kesarwani@sap.com>
2023-11-24 13:04:07 +0530

    urbwtytyyyruytp

--- pr_gen_service.py
+++ pr_gen_service.py
@@ -106,7 +106,7 @@ class PullRequestAutomationService(RemoteProgress):
         logger.info(f"Current dir: {curr_dir}")
         # print(file_to_sync)
         
-        dir_name = "".join(curr_dir, self.file_to_sync)
+        dir_name = "".join([curr_dir, self.file_to_sync])
         print(f'xyz:{dir_name}')
         # dir_name = "/home/runner/work/manser-test/manser-test/.github/workflows/git_mirror.yaml"
         # print("Hello")
